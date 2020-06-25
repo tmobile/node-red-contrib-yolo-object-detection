cad0d5fa09c8bdc65838f20b6be1448db4d513ea
Joe Bond <jbond@prokarma.com>
2020-06-25 10:31:14 -0700

    Update spawn with cwd to work with ServerAll.py

--- yolo-object-detection.js
+++ yolo-object-detection.js
@@ -28,16 +28,18 @@ module.exports = (RED) => {
         if (!python || python.killed) {
             node.debug('Starting python server process')
             node.debug('Current dir is: ' + __dirname)
-            python = spawn('python3', ['4_Serve/Serve.py'])
+            python = spawn('env/bin/python3', ['python-scripts/4_Serve/ServeAll.py'], {'cwd': __dirname})
 
             globalContext.set('yoloserver', python)
             node.log('Loaded Yoloserver')
 
             python.stdout.on('data', (data) => {
+                console.log(data.toString())
                 node.debug(data.toString())
             })
 
             python.stderr.on('data', (data) => {
+                console.log(data.toString())
                 if (data.toString().includes('Running on http://0.0.0.0')) {
                     serverStatus = { fill: 'green', shape: 'dot', text: 'connected' }
                     node.status(serverStatus)
